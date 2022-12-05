a4c30588602fe7723c19d9c130f0f74e32d18918
Mikhail Morozov <manah-94@mail.ru>
2022-12-05 08:06:45 +0100

    Refactored day 5. Now a bit less ugly.

--- FifthDay/Program.cs
+++ FifthDay/Program.cs
@@ -1,45 +1,44 @@
-﻿using System;
-using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 const string inputPath = @"..\..\..\input.txt";
 
-StringBuilder result = new();
-var stacks = new Dictionary<int, Stack<char>>();
+var stacksFirst = new Dictionary<int, Stack<char>>();
+var stacksSecond = new Dictionary<int, Stack<char>>();
+
 bool first = true;
 
-// 0 - 1
-// 1 - 5
-// 2 - 9
 using var reader = new StreamReader(inputPath);
 while (!reader.EndOfStream)
 {
     string? fullString = reader.ReadLine();
     if (string.IsNullOrEmpty(fullString)) break;
+
+    // Initializing necessary amount of stacks
     if (first)
     {
         for (int i = 0; i <= fullString.Length / 4; i++)
-        {
-            stacks[i] = new Stack<char>();
-        }
+            stacksFirst[i] = new Stack<char>();
+
         first = false;
     }
 
-    for (int i = 1; i < fullString.Length; i+=4)
+    for (int i = 1; i < fullString.Length; i += 4)
     {
         var index = (i - 1) / 4;
-        if (char.IsLetter(fullString[i])) stacks[index].Push(fullString[i]);
+
+        if (char.IsLetter(fullString[i]))
+            stacksFirst[index].Push(fullString[i]);
     }
 }
-for (int i = 0; i < stacks.Count; i++)
+
+// Reversing the stacks
+for (int i = 0; i < stacksFirst.Count; i++)
 {
-    var reversed = stacks[i];
-    stacks[i] = new();
-    foreach (var item in reversed)
-    {
-        stacks[i].Push(item);
-    }
+    var reversedStack = stacksFirst[i];
+    stacksFirst[i] = new(reversedStack);
+    stacksSecond[i] = new(reversedStack);
 }
+
 while (!reader.EndOfStream)
 {
     string? fullString = reader.ReadLine();
@@ -49,19 +48,20 @@ while (!reader.EndOfStream)
     var amount = int.Parse(list[1]);
     var from = int.Parse(list[2]);
     var to = int.Parse(list[3]);
-    var temp = new Stack<char>();
 
-    for (int i = 0; i < amount; i++)
-    {
-        temp.Push(stacks[from - 1].Pop());
-    }
-    for (int i = 0; i < amount; i++)
-    {
-        stacks[to-1].Push(temp.Pop());
-    }
+    // Solution for part one
+    for (int i = 0; i < amount; i++) stacksFirst[to - 1].Push(stacksFirst[from - 1].Pop());
+
+    // Solution for part two
+    var temp = new Stack<char>();
+    for (int i = 0; i < amount; i++) temp.Push(stacksSecond[from - 1].Pop());
+    for (int i = 0; i < amount; i++) stacksSecond[to - 1].Push(temp.Pop());
 }
-for (int i = 0; i < stacks.Count; i++)
-{
-    result.Append(stacks[i].Pop());
-}
-Console.WriteLine(result);
+
+var lettasFirst = stacksFirst.Select(pair => pair.Value.Pop());
+var lettasSecond = stacksSecond.Select(pair => pair.Value.Pop());
+
+var resultFirst = string.Concat(lettasFirst);
+var resultSecond = string.Concat(lettasSecond);
+
+Console.WriteLine($"Part one: {resultFirst}\nPart two: {resultSecond}");
