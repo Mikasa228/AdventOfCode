4bf3a81334811d76984d186064a02fece8193db7
Mikhail Morozov <manah-94@mail.ru>
2022-12-06 07:11:38 +0100

    Goin' fancy!

--- SixthDay/Program.cs
+++ SixthDay/Program.cs
@@ -9,23 +9,29 @@ while (!reader.EndOfStream)
     string? fullString = reader.ReadLine();
     if (fullString is null) throw new NullReferenceException();
 
-    resultFirst = CalculateResult(fullString, 4);
-    resultSecond = CalculateResult(fullString, 14);
+    resultFirst = CalculateResult(fullString, MarkerType.StartOfPacket);
+    resultSecond = CalculateResult(fullString, MarkerType.StartOfMessage);
 
     Console.WriteLine($"Part one: {resultFirst}\nPart two: {resultSecond}\n"); ;
 }
 
 
-static int CalculateResult(string fullString, int count)
+static int CalculateResult(string dataStreamBuffer, MarkerType markerType)
 {
     var buffer = new Queue<char>();
-    for (int i = 0; i < fullString.Length; i++)
+    for (int markerPosition = 0; markerPosition < dataStreamBuffer.Length; markerPosition++)
     {
-        buffer.Enqueue(fullString[i]);
-        if (buffer.Count < count) continue;
-        if (buffer.Distinct().ToList().Count == count) return i + 1;
+        buffer.Enqueue(dataStreamBuffer[markerPosition]);
+        if (buffer.Count < (int)markerType) continue;
+        if (buffer.Distinct().ToList().Count == (int)markerType) return markerPosition + 1;
         buffer.Dequeue();
     }
 
     return -1;
+}
+
+enum MarkerType
+{
+    StartOfPacket = 4,
+    StartOfMessage = 14
 }
