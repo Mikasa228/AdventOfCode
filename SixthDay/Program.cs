8f0d5a3226c2db67bb434a978d179e9d3b0482b3
Mikhail Morozov <manah-94@mail.ru>
2022-12-06 06:49:55 +0100

    Added initial solution for day 6. It's a miracle it worked, now that I look at it.

---
+++ SixthDay/Program.cs
@@ -0,0 +1,32 @@
+﻿const string inputPath = @"..\..\..\input.txt";
+
+var result = 0;
+
+using var reader = new StreamReader(inputPath);
+while (!reader.EndOfStream)
+{
+    string? fullString = reader.ReadLine();
+    if (fullString is null) throw new NullReferenceException();
+
+    var buffer = new Queue<char>();
+    for (int i = 0; i < fullString.Length; i++)
+    {
+        if (buffer.Count == 14)
+        {
+            buffer.Dequeue();
+
+        }
+
+        buffer.Enqueue(fullString[i]);
+        if (buffer.Count == 14)
+        {
+            if (buffer.Distinct().ToList().Count == 14)
+            {
+                result = i + 1;
+                break;
+            }
+        }
+    }
+}
+
+Console.WriteLine(result); ;
