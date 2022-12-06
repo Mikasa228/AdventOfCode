c6a19309f1c1107903c65e1aae8a8c9e47e03213
Mikhail Morozov <manah-94@mail.ru>
2022-12-06 06:57:08 +0100

    Refactored day 6

--- SixthDay/Program.cs
+++ SixthDay/Program.cs
@@ -1,6 +1,7 @@
 ﻿const string inputPath = @"..\..\..\input.txt";
 
-var result = 0;
+int resultFirst;
+int resultSecond;
 
 using var reader = new StreamReader(inputPath);
 while (!reader.EndOfStream)
@@ -8,25 +9,23 @@ while (!reader.EndOfStream)
     string? fullString = reader.ReadLine();
     if (fullString is null) throw new NullReferenceException();
 
+    resultFirst = CalculateResult(fullString, 4);
+    resultSecond = CalculateResult(fullString, 14);
+
+    Console.WriteLine($"Part one: {resultFirst}\nPart two: {resultSecond}\n"); ;
+}
+
+
+static int CalculateResult(string fullString, int count)
+{
     var buffer = new Queue<char>();
     for (int i = 0; i < fullString.Length; i++)
     {
-        if (buffer.Count == 14)
-        {
-            buffer.Dequeue();
-
-        }
-
         buffer.Enqueue(fullString[i]);
-        if (buffer.Count == 14)
-        {
-            if (buffer.Distinct().ToList().Count == 14)
-            {
-                result = i + 1;
-                break;
-            }
-        }
+        if (buffer.Count < count) continue;
+        if (buffer.Distinct().ToList().Count == count) return i + 1;
+        buffer.Dequeue();
     }
-}
 
-Console.WriteLine(result); ;
+    return -1;
+}
