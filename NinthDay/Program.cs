f4d9ad02790214af4bcb5d6388daf3d0233ef61f
Mikhail Morozov <manah-94@mail.ru>
2022-12-09 11:05:05 +0100

    Added solution for day 9 with a certain unnecessary overcomplication

---
+++ NinthDay/Program.cs
@@ -0,0 +1,169 @@
+﻿namespace NinthDay;
+
+class Program
+{
+    static bool fastForward = false;
+
+    static void Main()
+    {
+        Console.Write("Press Enter to start...");
+        Console.ReadLine();
+        Console.Clear();
+
+        var resultsTask = Task.Factory.StartNew(() => Run());
+
+        Console.WriteLine("Press Enter to fast-forward..");
+        while (!resultsTask.IsCompleted)
+        {
+            var key = Console.ReadKey();
+            if (key.Key.ToString() == "Enter")
+            {
+                fastForward = true;
+                break;
+            }
+        }
+
+        var results = resultsTask.Result;
+
+        Console.SetCursorPosition(0, 0);
+        Console.Write("Press Enter to see the results...");
+        Console.ReadLine();
+
+        Console.Clear();
+        Console.WriteLine($"Part one: {results[0]}\nPart two: {results[1]}\n");
+    }
+
+    static List<int> Run()
+    {
+        const string inputPath = @"..\..\..\input.txt";
+
+        // Initialization for part one
+        var visitedFirst = new List<string>();
+
+        var firstHeadPosition = new Point(0, 0);
+        var firstTailPosition = new Point(0, 0);
+
+
+        visitedFirst.Add(firstTailPosition.ToString());
+
+        // Initialization for part two
+        var visitedSecond = new List<string>();
+
+        var secondHeadPosition = new Point(0, 0);
+        var secondTailPosition = new Point(0, 0);
+
+        var rope = new List<Point>();
+        rope.Add(secondHeadPosition);
+        for (int _ = 0; _ < 8; _++)
+        {
+            rope.Add(new Point(0, 0));
+        }
+        rope.Add(secondTailPosition);
+
+        var renderer = new RopeRenderer(rope);
+        renderer.RenderRope();
+
+        visitedSecond.Add(secondTailPosition.ToString());
+
+        using var reader = new StreamReader(inputPath);
+        while (!reader.EndOfStream)
+        {
+            string? fullString = reader.ReadLine();
+            if (fullString is null) throw new NullReferenceException();
+
+            var input = fullString.Split(' ');
+            var direction = input[0];
+            var distance = int.Parse(input[1]);
+
+            for (int i = 0; i < distance; i++)
+            {
+                // Solution for part one
+                firstHeadPosition.Move(direction);
+                renderer.RenderRope();
+                if (!fastForward) Thread.Sleep(1);
+                firstTailPosition.CatchUp(firstHeadPosition);
+                if (!visitedFirst.Contains(firstTailPosition.ToString())) visitedFirst.Add(firstTailPosition.ToString());
+
+                // Solution for part two
+                secondHeadPosition.Move(direction);
+                for (int j = 1; j < rope.Count; j++)
+                {
+                    rope[j].CatchUp(rope[j - 1]);
+
+                }
+                if (!visitedSecond.Contains(secondTailPosition.ToString())) visitedSecond.Add(secondTailPosition.ToString());
+            }
+        }
+
+        return new List<int>()
+        {
+            visitedFirst.Count,
+            visitedSecond.Count
+        };
+    }
+}
+
+
+public class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public void Move(string direction)
+    {
+        switch (direction)
+        {
+            case "L":
+                X--;
+                break;
+            case "R":
+                X++;
+                break;
+            case "U":
+                Y++;
+                break;
+            case "D":
+                Y--;
+                break;
+            default:
+                break;
+        }
+    }
+
+    internal void CatchUp(Point coordinate)
+    {
+        var vectorX = coordinate.X - X;
+        var vectorY = coordinate.Y - Y;
+
+        var directionX = Math.Sign(vectorX) == -1 ? -1 : 1;
+        var directionY = Math.Sign(vectorY) == -1 ? -1 : 1;
+
+        if (directionX * vectorX > 1 && coordinate.Y != Y
+            || directionY * vectorY > 1 && coordinate.X != X)
+        {
+            X += directionX;
+            Y += directionY;
+        }
+
+        else if (directionX * vectorX > 1)
+        {
+            X += directionX;
+        }
+
+        else if (directionY * vectorY > 1)
+        {
+            Y += directionY;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{X} : {Y}";
+    }
+}
---
+++ NinthDay/Renderer.cs
@@ -0,0 +1,59 @@
+﻿namespace NinthDay;
+
+internal class RopeRenderer
+{
+    private readonly List<PointRenderer> renderers = new();
+    internal RopeRenderer(List<Point> points)
+    {
+        foreach (var point in points)
+        {
+            renderers.Add(new PointRenderer(point));
+        }
+    }
+    internal void RenderRope()
+    {
+        for (int i = renderers.Count - 1; i >= 0; i--)
+        {
+            var identifier = i switch
+            {
+                9 => "8",
+                0 => "э",
+                _ => "о"
+            };
+            renderers[i].Render(identifier);
+        }
+    }
+}
+
+internal class PointRenderer
+{
+    private readonly Point _coordinate;
+
+    private int _previousX = 0;
+    private int _previousY = 0;
+
+    private const int _offsetX = 105;
+    private const int _offsetY = 280;
+
+    private const double coeffX = 52d / 156d;
+    private const double coeffY = 196d / 280d;
+
+    internal PointRenderer(Point coordinate)
+    {
+        _coordinate = coordinate;
+    }
+    internal void Render(string identifier)
+    {
+        Console.CursorVisible = false;
+        Console.SetCursorPosition(ModifyY(_previousY), ModifyX(_previousX));
+        var placeholder = identifier == "8" ? "." : " ";
+        Console.Write(placeholder);
+        Console.SetCursorPosition(ModifyY(_coordinate.Y), ModifyX(_coordinate.X));
+        Console.Write(identifier);
+        _previousX = _coordinate.X;
+        _previousY = _coordinate.Y;
+    }
+
+    private static int ModifyX(int x) => (int)((x + _offsetX) * coeffX) > 51 ? 51 : (int)((x + _offsetX) * coeffX);
+    private static int ModifyY(int y) => (int)((y + _offsetY) * coeffY) > 195 ? 195 : (int)((y + _offsetY) * coeffY);
+}
