1d9ae731e7587d37d26df74827121e73162319e9
Mikhail Morozov <manah-94@mail.ru>
2022-12-10 10:04:05 +0100

    Minor refactoring

--- TenthDay/Program.cs
+++ TenthDay/Program.cs
@@ -4,19 +4,18 @@ namespace TenthDay;
 
 class Program
 {
-    static int cycleNumber = 1;
+    const string inputPath = @"..\..\..\input.txt";
+
     static int resultFirst = 0;
 
+    static int cycleNumber = 0;
     static int x = 1;
-    static int counter = 1;
 
     static readonly List<string> imageLines = new();
-    static readonly StringBuilder currentLine = new();
+    static readonly StringBuilder currentImageLine = new();
 
     static void Main()
     {
-        const string inputPath = @"..\..\..\input.txt";
-
         using var reader = new StreamReader(inputPath);
         Draw();
         while (!reader.EndOfStream)
@@ -32,45 +31,35 @@ class Program
             }
 
             IncreaseCycleAndProcess();
-            x += int.Parse(command[1]);
             IncreaseCycleAndProcess();
+            x += int.Parse(command[1]);
+        }
 
-        }
         Console.WriteLine($"Part one: {resultFirst}\nPart two:");
-        foreach (var line in imageLines)
-        {
-            Console.WriteLine(line);
-        }
+        imageLines.ForEach(line => Console.WriteLine(line));
     }
 
     static void IncreaseCycleAndProcess()
     {
         cycleNumber++;
         Draw();
-        if (cycleNumber == 20 || (cycleNumber + 20) % (counter * 40) == 0)
+        if ((cycleNumber + 20) % 40 == 0)
         {
             resultFirst += (x * cycleNumber);
-            counter++;
         }
     }
 
     static void Draw()
     {
         var position = (cycleNumber - 1) % 40;
-        if (position == x - 1
-            || position == x
-            || position == x + 1)
+
+        var symbol = Math.Abs(position - x) <= 1 ? '#' : ' ';
+        currentImageLine.Append(symbol);
+
+        if (currentImageLine.Length == 40)
         {
-            currentLine.Append('#');
-        }
-        else
-        {
-            currentLine.Append('.');
-        }
-        if (currentLine.Length == 40)
-        {
-            imageLines.Add(currentLine.ToString());
-            currentLine.Clear();
+            imageLines.Add(currentImageLine.ToString());
+            currentImageLine.Clear();
         }
     }
 }
