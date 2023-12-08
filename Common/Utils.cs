1d9557ecf21d1c3c5b5c5de2a2c8eaee57667558
Mikhail Morozov <manah-94@mail.ru>
2023-12-08 16:01:33 +0100

    Adding some utility methods for hype calculations

---
+++ Common/Utils.cs
@@ -0,0 +1,21 @@
+﻿namespace Common;
+
+public static class Utils
+{
+    public static long GCD(long n1, long n2)
+    {
+        if (n2 == 0)
+        {
+            return n1;
+        }
+        else
+        {
+            return GCD(n2, n1 % n2);
+        }
+    }
+
+    public static long LCM(List<long> numbers)
+    {
+        return numbers.Aggregate((S, val) => S * val / GCD(S, val));
+    }
+}
